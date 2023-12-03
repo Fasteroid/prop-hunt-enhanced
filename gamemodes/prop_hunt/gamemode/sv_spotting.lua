c4fe9954390b81e543083b5c7af3658eb67f2529
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2023-12-03 15:49:31 -0500

    lag compensate everything

--- gamemodes/prop_hunt/gamemode/sv_spotting.lua
+++ gamemodes/prop_hunt/gamemode/sv_spotting.lua
@@ -14,50 +14,58 @@ local function pickRandom(tbl)
 end
 
 do
-    PHE.PositionHistory = {}
+    PHE.PlayerHistory = {}
 
     -- Register the player's Position at every PlayerTick
-    hook.Add("PlayerTick", "PH:Infinity.PositionHistory", function(ply, mv)
-        if ply:Team() ~= TEAM_PROPS then return end
+    hook.Add("PlayerTick", "PH:Infinity.PlayerHistory", function(ply, mv)
+        PHE.PlayerHistory[ply] = PHE.PlayerHistory[ply] or {}
+        local history = PHE.PlayerHistory[ply]
 
-        PHE.PositionHistory[ply] = PHE.PositionHistory[ply] or {}
-
-        if table.Count(PHE.PositionHistory[ply]) > MAX_POS_HISTORY then
-            table.remove(PHE.PositionHistory[ply], 1)
+        if #history > MAX_POS_HISTORY then
+            table.remove(history, 1)
         end
 
-        table.insert(PHE.PositionHistory[ply], ply:WorldSpaceCenter())
+        local data = {}
+        if ply:Team() == TEAM_PROPS then
+            data.boxcenter = ply:WorldSpaceCenter()
+            data.speed     = ply:GetVelocity():LengthSqr()
+        else
+            data.shootpos  = ply:GetShootPos()
+            data.aimvector = ply:GetAimVector()
+        end
+
+        history[#history + 1] = data
     end)
 
     -- Cleanup the PositionHistory to save a few bytes :skullemoji:
     hook.Add("PlayerDisconnected", "PH:Infinity.HistoryCleaner", function(ply)
-        PHE.PositionHistory[ply] = nil
+        PHE.PlayerHistory[ply] = nil
     end)
 
     -- Reset the player history every round (every respawn)
     hook.Add("PlayerSpawn", "PH:Infinity.HistoryCleaner", function(ply)
-        PHE.PositionHistory[ply] = {}
+        PHE.PlayerHistory[ply] = {}
     end)
 end
 
 local player_meta = FindMetaTable("Player")
 local tick_interval = engine.TickInterval()
 
-function player_meta:GetLatentPosFrom(spotter)
-    local history_size = #PHE.PositionHistory[self]
-    local ping_ind = math.floor( spotter:Ping() * tick_interval )
-    ping_ind = history_size - math.min(ping_ind, history_size)
-    local pose = PHE.PositionHistory[self] and PHE.PositionHistory[self][ ping_ind ]
-    return pose
+function player_meta:GetLaggedData( ping, key )
+    local history = PHE.PlayerHistory[self]
+    local ping_ind = math.floor( ping * tick_interval ) -- where does the 6 come from? who cares?!
+    ping_ind = #history - math.min(ping_ind, #history)
+    local data = history[ping_ind]
+    return data[key]
 end
 
-local function checkLineOfSight(point, ent)
+local function checkLineOfSight(hunter_pos, prop_pos, filter_ent)
     local filter = player.GetAll()
-    table.insert(filter, ent.ph_prop)
+    table.insert(filter, filter_ent)
 
     local tr = util.TraceLine({
-        start = point,
-        endpos = ent:NearestPoint(point),
+        start = hunter_pos,
+        endpos = prop_pos,
         filter = filter
     })
 
@@ -69,27 +77,31 @@ local function AttemptSpotting(hunter)
     local bestscore = 9e9
 
     local player_count = player.GetCount()
-    local players = player.GetAll()
+    local players      = player.GetAll()
 
     for i = 1, player_count do
         local ply = players[i]
         if ply:Team() ~= TEAM_PROPS then continue end -- nope
         if not ply:Alive() then continue end -- nah
-        if ply:GetVelocity():LengthSqr() < 3600 then continue end -- too stealthy
 
-        if not checkLineOfSight(hunter:GetShootPos(), ply) then continue end -- no
+        local lag = hunter:Ping()
+
+        local aim_hunter = hunter:GetLaggedData(lag, "aimvector")
+        local pos_hunter = hunter:GetLaggedData(lag, "shootpos")
+        local pos_prop   = ply:GetLaggedData(lag, "boxcenter")
+        local speed_prop = ply:GetLaggedData(lag, "speed")
+
+        if speed_prop < 2800 then continue end -- too stealthy
+
+        if not checkLineOfSight(pos_hunter, pos_prop, ply.ph_prop) then continue end -- no
 
         ply.nextSpot = ply.nextSpot or 0
 
         if ply.nextSpot > CurTime() then continue end -- already spotted
 
-        local pos_ply = ply:GetLatentPosFrom(hunter)
-        if not pos_ply then return end
+        if (pos_prop - pos_hunter):GetNormalized():Dot( aim_hunter ) < MIN_DOT_PROD then continue end -- get better aim dude
 
-        local pos_hunter = hunter:GetShootPos()
-        if (pos_ply - pos_hunter):GetNormalized():Dot(hunter:GetAimVector()) < MIN_DOT_PROD then continue end -- get better aim dude
-
-        local score = pos_ply:DistToSqr(hunter:GetShootPos())
+        local score = pos_prop:DistToSqr(pos_hunter)
 
         if score < bestscore then
             bestscore = score
